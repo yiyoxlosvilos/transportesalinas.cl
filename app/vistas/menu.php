32a75af7deb5928472f785e6ad19be270fd24a4e
Mauricio Villalobos Tordecilla <m.villalobos.tord@gmail.com>
2024-12-13 14:46:00 -0300

    Guardando cambios locales en menu.php

--- app/vistas/menu.php
+++ app/vistas/menu.php
@@ -28,10 +28,14 @@
                 <span class="nav_logo-name">Centro Comercio</span>
             </a>
                 <div class="nav_list">
-                    <!-- <a href="centro_costo" class="nav_link <?= Utilidades::activo_get($_GET["action"], 'centro_costo'); ?>" >
+                    <a href="centro_costo" class="nav_link <?= Utilidades::activo_get($_GET["action"], 'centro_costo'); ?>" >
                         <i class="bi bi-badge-cc nav_icon"></i>
                         <span class="nav_name">Centro Costo</span>
-                    </a> -->
+                    </a>
+                    <a href="viajes" class="nav_link <?= Utilidades::activo_get($_GET["action"], 'bitacora'); ?>" <?= $display ?>>
+                        <i class="bi bi-calendar2-week nav_icon"></i>
+                        <span class="nav_name">Viajes</span>
+                    </a>
                     <a href="bitacora" class="nav_link <?= Utilidades::activo_get($_GET["action"], 'bitacora'); ?>" <?= $display ?>>
                         <i class="bi bi-calendar2-week nav_icon"></i>
                         <span class="nav_name">Bitacora</span>

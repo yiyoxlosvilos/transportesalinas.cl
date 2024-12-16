ca57fb948c5a878f888715d011ba54d64f21d648
Mauricio Villalobos Tordecilla <m.villalobos.tord@gmail.com>
2024-12-16 18:09:39 -0300

    Viajes

--- app/vistas/viajes/php/validador.php
+++ app/vistas/viajes/php/validador.php
@@ -53,7 +53,7 @@
 					    	'.$bodega->select_productos_multiple(1, 1).' 
 					    </div>
 					    <div class="col-md-3">
-					        <button class="btn btn-primary w-lg waves-effect waves-light w-100 waves-effect waves-light " type="button" onclick="asignar_productos_cotizacion()"><i class="bi bi-search"></i>
+					        <button class="btn btn-primary waves-effect waves-light waves-light " type="button" onclick="asignar_productos_cotizacion()"><i class="bi bi-search"></i>
 					        </button>
 					    </div>
 					    <hr class="mt-2 mb-3"/>

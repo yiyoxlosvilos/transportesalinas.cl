8cf506c7c5b6c2f1ddef30e04b76451a3f9d9a75
Mauricio Villalobos Tordecilla <m.villalobos.tord@gmail.com>
2024-12-17 02:50:43 -0300

    Viajes

--- app/controlador/viajesControlador.php
+++ app/controlador/viajesControlador.php
@@ -634,11 +634,17 @@ ini_set('error_log', __DIR__ . '/php_errors.log');
 			$recursos 	= new Recursos();
 			$hoy 		= Utilidades::fecha_hoy();
 
-			$sql = $this->insert_query("INSERT INTO fletes(fle_venta, fle_producto, fle_rampla, fle_valor, fle_guia, fle_origen, fle_destino, fle_carga, fle_arribo, fle_chofer, fle_estadia, fle_glosa, fle_creacion, fle_estado, fle_descarga, fle_acompanante, fle_descuento, fle_estado_pago, fle_fecha_pago, fle_clientes) VALUES('$idServicio', '$idProducto', '$inputRampla', '$inputFlete', '$inputGuia', '$inputOrigen', '$inputDestino', '$inputCarga', '$inputArribo', '$inputTrabajador', '$inputMontoEstadia', '$inputGlosa', '$hoy', 1, '$inputDescarga', '$inputAcompanante_items', '$inputDescuento', , '$tipos_estados_pagos', '$inputFechaPago', '$clientes')");
+			try {
+				$sql = $this->insert_query("INSERT INTO fletes(fle_venta, fle_producto, fle_rampla, fle_valor, fle_guia, fle_origen, fle_destino, fle_carga, fle_arribo, fle_chofer, fle_estadia, fle_glosa, fle_creacion, fle_estado, fle_descarga, fle_acompanante, fle_descuento, fle_estado_pago, fle_fecha_pago, fle_clientes) VALUES('$idServicio', '$idProducto', '$inputRampla', '$inputFlete', '$inputGuia', '$inputOrigen', '$inputDestino', '$inputCarga', '$inputArribo', '$inputTrabajador', '$inputMontoEstadia', '$inputGlosa', '$hoy', 1, '$inputDescarga', '$inputAcompanante_items', '$inputDescuento', , '$tipos_estados_pagos', '$inputFechaPago', '$clientes')");
 
-			//$recursos->cambiar_producto_estado($idProducto, 1);
+				//$recursos->cambiar_producto_estado($idProducto, 1);
 
-			return $sql;
+				return $sql;
+			} catch (Exception $e) {
+				return $e;
+			}
+
+			
 		}
 
 		public function formulario_editar_flete($idFlete){

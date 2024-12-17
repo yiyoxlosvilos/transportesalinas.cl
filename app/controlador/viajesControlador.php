930c5e7f5b0c518314ee7d4c484b81ecec669e60
Mauricio Villalobos Tordecilla <m.villalobos.tord@gmail.com>
2024-12-17 03:25:12 -0300

    Viajes

--- app/controlador/viajesControlador.php
+++ app/controlador/viajesControlador.php
@@ -261,7 +261,7 @@ ini_set('error_log', __DIR__ . '/php_errors.log');
 					for ($d=0; $d < count($explorar_acompanante); $d++) {
 						$acompa = $recursos->datos_trabajador($explorar_acompanante[$d]);
 
-						$acompanantes .= '<div class="row border">
+						$acompanantes .= '<div class="row border m-1">
 										<div class="col"><b>Acompañante '.$cont_acompanantes++.':</b></div>
 										<div class="col">'.ucfirst(ucwords($acompa[0]['tra_nombre'])).'</div>
 									</div>';
@@ -271,8 +271,8 @@ ini_set('error_log', __DIR__ . '/php_errors.log');
 				}
 
 				$html  .= '<tr>
-								<td class="p-1"><small>
-									<div class="row">
+								<td><small>
+									<div class="row m-1">
 										<div class="col"><b>Conductor:</b></div>
 										<div class="col">'.ucfirst(ucwords($trabajador[0]['tra_nombre'])).'</div>
 									</div>

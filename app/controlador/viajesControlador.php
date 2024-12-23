452c5dfe79b7e980dc0b91a31d008a35f7656d51
Mauricio Villalobos Tordecilla <m.villalobos.tord@gmail.com>
2024-12-23 16:33:34 -0300

    grabar

--- app/controlador/viajesControlador.php
+++ app/controlador/viajesControlador.php
@@ -287,10 +287,8 @@ ini_set('error_log', __DIR__ . '/php_errors.log');
 										<div class="row">
 											<div class="col-6">
 												<div class="row">
-													<div class="col-6"><b>&nbsp;Origen:</b></div>
-													<div class="col-6">'.$nombre_origen.'</div>
-													<div class="col-6"><b>&nbsp;Destino:</b></div>
-													<div class="col-6">'.$nombre_destino.'</div>
+													<div class="col"><b>&nbsp;Origen:</b>< '.$nombre_origen.'</div>
+													<div class="col"><b>&nbsp;Destino:</b> '.$nombre_destino.'</div>
 												</div>
 											</div>
 											<div class="col-6">

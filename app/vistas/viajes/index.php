c1402ed57edd5fc4b822c083b771c17cc2fa4a57
Mauricio Villalobos Tordecilla <m.villalobos.tord@gmail.com>
2024-12-23 16:28:43 -0300

    grabar

--- app/vistas/viajes/index.php
+++ app/vistas/viajes/index.php
@@ -108,7 +108,7 @@
                       <div class="tab-pane fade active show" id="overview-project" role="tabpanel" aria-labelledby="overview-project-tab">
                         <div class="row">
                           <div class="col-8">
-                            <h3 class="text-primary mb-2"><span class="mdi mdi-format-list-bulleted"></span> Detalles Viajes</h3>
+                            <h3 class="text-dark mb-2"><span class="mdi mdi-format-list-bulleted"></span> Detalles Viajes</h3>
                           </div>
                           <div class="col">
                             <button class="btn btn-sm btn-dark" onclick="traer_menu('asignar_producto')"><i class="fas fa-shipping-fast text-white"></i> <span class="ocultar">Nuevo Viaje</span></button>

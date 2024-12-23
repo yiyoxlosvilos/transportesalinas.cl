3e3477f7d01877d3a8857411724a1b863ded4ea9
Mauricio Villalobos Tordecilla <m.villalobos.tord@gmail.com>
2024-12-23 17:54:54 -0300

    grabar

--- app/controlador/viajesControlador.php
+++ app/controlador/viajesControlador.php
@@ -773,107 +773,92 @@ ini_set('error_log', __DIR__ . '/php_errors.log');
 			
 		}
 
-		public function formulario_editar_flete($idFlete){
-			$recursos    = new Recursos();
-			$datos_flete = $recursos->datos_fletes_id($idFlete);
+		public function formulario_editar_flete($idFlete) {
+		    $recursos    = new Recursos();
+		    $datos_flete = $recursos->datos_fletes_id($idFlete);
 
-			$html 	     = '';
-			$data        = array();
-			$productos   = '';
-			$productos_id= '';
+		    $html        = '';
+		    $data        = array();
+		    $productos   = '';
+		    $productos_id = '';
 
-			for ($i = 0; $i < count($datos_flete); $i++) {
-				$datos_nombre = $recursos->datos_productos($datos_flete[$i]['fle_producto']);
+		    for ($i = 0; $i < count($datos_flete); $i++) {
+		        $datos_nombre = $recursos->datos_productos($datos_flete[$i]['fle_producto']);
 
-				$html .= '<div class="row shadow-sm">
-							<div class="col-sm-4   bg-white ">
-								<h6>Tracto:</h6>
-								
-									'.ucfirst($datos_nombre[0]['prod_cli_producto']).' - '.ucwords($datos_nombre[0]['prod_cli_patente']).'
-						  			<input type="hidden" name="idProducto" id="idProducto" value="'.$datos_nombre[0]['prod_cli_id'].'">
-						  		
-							</div>
-							<div class="col-sm-4   bg-white ">
-								<h6>Valor Flete:</h6>
-								
-									<input type="text" class="form-control shadow" id="inputFlete" placeholder="Valor" autocomplete="off" value="'.$datos_flete[$i]['fle_valor'].'">
-						  		
-							</div>
-							<div class="col-sm-4   bg-white ">
-								<h6>N&deg; Guia:</h6>
-								
-									<input type="text" class="form-control shadow" id="inputGuia" placeholder="N&deg; Guia" autocomplete="off" value="'.$datos_flete[$i]['fle_guia'].'">
-						  		
-							</div>
-							<div class="col-sm-6   bg-white ">
-								<h6>Origen:</h6>
-								
-									'.$recursos->seleccionar_localidad2($idFlete, 'inputOrigen', 1).'
-						  		
-							</div>
-							<div class="col-sm-6   bg-white ">
-								<h6>Destino:</h6>
-								
-									'.$recursos->seleccionar_localidad2($idFlete, 'inputDestino', 2).'
-						  		
-							</div>
-							<div class="col-sm-4   bg-white ">
-								<h6>Fecha Carga:</h6>
-								
-									<input type="date" class="form-control shadow" id="inputCarga" value="'.$datos_flete[$i]['fle_carga'].'" autocomplete="off" >
-						  		
-							</div>
-							<div class="col-sm-4   bg-white ">
-								<h6>Fecha Arribo:</h6>
-								
-									<input type="date" class="form-control shadow" id="inputArribo" value="'.$datos_flete[$i]['fle_arribo'].'" autocomplete="off" >
-						  		
-							</div>
-							<div class="col-sm-4   bg-white ">
-								<h6>Fecha Descarga:</h6>
-								
-									<input type="date" class="form-control shadow" id="inputDescarga" value="'.$datos_flete[$i]['fle_descarga'].'" autocomplete="off" >
-						  		
-							</div>
-							<div class="col-sm-4   bg-white ">
-								<h6>Chofer:</h6>
-								
-									'.$recursos->seleccionar_trabajadores($datos_flete[$i]['fle_chofer']).'
-						  		
-							</div>
-							<div class="col-sm-4   bg-white ">
-								<h6>Semirremolque:</h6>
-								<div class="row">
-							  		<div class="col"   id="semirremolque">
-										'.$recursos->seleccionar_productos_general(2, $datos_flete[$i]['fle_rampla']).'
-							  		</div>
-								</div>
-							</div>
-							<div class="col-sm-4   bg-white ">
-								<h6>Estadia:</h6>
-								<div class="row">
-							  		<div class="col" id="estadia">
-										<input type="text" class="form-control shadow" id="inputMontoEstadia" placeholder="Monto Estadia" autocomplete="off" value="'.$datos_flete[$i]['fle_estadia'].'">
-							  		</div>
-								</div>
-							</div>
+		        $html .= '<div class="row shadow-sm">
+		            <div class="col-sm-4 bg-white">
+		                <h6>Tracto:</h6>
+		                <span class="text-dark">
+		                    ' . ucfirst($datos_nombre[0]['prod_cli_producto']) . ' - ' . ucwords($datos_nombre[0]['prod_cli_patente']) . '
+		                    <input type="hidden" name="idProducto" value="' . $datos_nombre[0]['prod_cli_id'] . '">
+		                </span>
+		            </div>
+		            <div class="col-sm-4 bg-white">
+		                <h6>Valor Flete:</h6>
+		                <input type="text" class="form-control shadow" id="inputFlete" placeholder="Valor" autocomplete="off" value="' . $datos_flete[$i]['fle_valor'] . '">
+		            </div>
+		            <div class="col-sm-4 bg-white">
+		                <h6>N&deg; Guía:</h6>
+		                <input type="text" class="form-control shadow" id="inputGuia" placeholder="N&deg; Guía" autocomplete="off" value="' . $datos_flete[$i]['fle_guia'] . '">
+		            </div>
+		            <div class="col-sm-6 bg-white">
+		                <h6>Origen:</h6>
+		                ' . $recursos->seleccionar_localidad2($idFlete, 'inputOrigen', 1) . '
+		            </div>
+		            <div class="col-sm-6 bg-white">
+		                <h6>Destino:</h6>
+		                ' . $recursos->seleccionar_localidad2($idFlete, 'inputDestino', 2) . '
+		            </div>
+		            <div class="col-sm-4 bg-white">
+		                <h6>Fecha Carga:</h6>
+		                <input type="date" class="form-control shadow" id="inputCarga" value="' . $datos_flete[$i]['fle_carga'] . '" autocomplete="off">
+		            </div>
+		            <div class="col-sm-4 bg-white">
+		                <h6>Fecha Arribo:</h6>
+		                <input type="date" class="form-control shadow" id="inputArribo" value="' . $datos_flete[$i]['fle_arribo'] . '" autocomplete="off">
+		            </div>
+		            <div class="col-sm-4 bg-white">
+		                <h6>Fecha Descarga:</h6>
+		                <input type="date" class="form-control shadow" id="inputDescarga" value="' . $datos_flete[$i]['fle_descarga'] . '" autocomplete="off">
+		            </div>
+		            <div class="col-sm-4 bg-white">
+		                <h6>Chofer:</h6>
+		                ' . $recursos->seleccionar_trabajadores($datos_flete[$i]['fle_chofer']) . '
+		            </div>
+		            <div class="col-sm-4 bg-white">
+		                <h6>Semirremolque:</h6>
+		                <div class="row">
+		                    <div class="col" id="semirremolque">
+		                        ' . $recursos->seleccionar_productos_general(2, $datos_flete[$i]['fle_rampla']) . '
+		                    </div>
+		                </div>
+		            </div>
+		            <div class="col-sm-4 bg-white">
+		                <h6>Estadía:</h6>
+		                <div class="row">
+		                    <div class="col" id="estadia">
+		                        <input type="text" class="form-control shadow" id="inputMontoEstadia" placeholder="Monto Estadía" autocomplete="off" value="' . $datos_flete[$i]['fle_estadia'] . '">
+		                    </div>
+		                </div>
+		            </div>
+		            <div class="col-sm-12 bg-white">
+		                <h6>Descripción del Trabajo:</h6>
+		                <textarea class="form-control shadow" id="inputGlosa" placeholder="Glosa" rows="5">' . $datos_flete[$i]['fle_glosa'] . '</textarea>
+		            </div>
+		            <div class="col-sm-12 bg-white">
+		                <button type="button" class="btn btn-success form-control shadow" onclick="editar_flete(' . $datos_flete[$i]['fle_id'] . ')">
+		                    Editar&nbsp;<i class="fas fa-truck-moving text-dark"></i>
+		                </button>
+		                <button type="button" class="btn btn-danger form-control shadow" onclick="quitar_flete(' . $datos_flete[$i]['fle_id'] . ')">
+		                    Quitar&nbsp;<i class="far fa-window-close text-dark"></i>
+		                </button>
+		            </div>
+		        </div>';
+		    }
 
-							<div class="col-sm-12   bg-white ">
-								<h6>Descripción del Trabajo:</h6>
-								
-									<textarea class="form-control shadow" id="inputGlosa" placeholder="Glosa" row="5">'.$datos_flete[$i]['fle_glosa'].'</textarea>
-						  	
-							</div>
-
-							<button type="button" id="grabar" class="btn btn-success form-control shadow" onclick="editar_flete('.$datos_flete[$i]['fle_id'].')">Editar&nbsp;<i class="fas fa-truck-moving text-dark"></i></button>
-
-							<button type="button" id="grabar" class="btn btn-danger form-control shadow" onclick="quitar_flete('.$datos_flete[$i]['fle_id'].')">Quitar&nbsp;<i class="far fa-window-close text-dark"></i></button>
-
-					  	</div>';
-			}
-
-			return $html;
+		    return $html;
 		}
+
 
 		public function editar_flete($idFlete, $idProducto, $inputFlete, $inputGuia, $inputOrigen, $inputDestino, $inputCarga, $inputArribo, $inputTrabajador, $inputRampla, $inputMontoEstadia, $inputGlosa, $inputDescarga){
 			$hoy = Utilidades::fecha_hoy();

a1f7c8fa0e6883ecfc2a4fbe01b7ce6ba658035a
Mauricio Villalobos Tordecilla <m.villalobos.tord@gmail.com>
2024-12-23 17:46:21 -0300

    grabar

--- app/vistas/viajes/php/panel_flete.php
+++ app/vistas/viajes/php/panel_flete.php
@@ -122,10 +122,18 @@
                       <div class="tab-pane fade" id="team-project" role="tabpanel" aria-labelledby="team-project-tab">
                         <div class="row">
                           <div class="col-8">
+                            <h3 class="text-dark mb-2"><span class="mdi mdi-format-list-bulleted"></span> Ingresar Pagos</h3>
+                          </div>    
+                        </div>
+                      </div>
+                      <div class="tab-pane fade active show" id="attachment" role="tabpanel" aria-labelledby="attachment-tab">
+                        <div class="row">
+                          <div class="col-8">
                             <h3 class="text-dark mb-2"><span class="mdi mdi-format-list-bulleted"></span> Descargar e Imprimir</h3>
                           </div>    
                         </div>
                       </div>
+
                     </div>
                   </div>
                 </div>

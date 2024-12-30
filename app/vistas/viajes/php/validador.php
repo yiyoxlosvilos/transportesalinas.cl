9b1398a3dd6c8f9bfe7846dc2fbda4ccd968cdf8
Mauricio Villalobos Tordecilla <m.villalobos.tord@gmail.com>
2024-12-30 17:17:43 -0300

    nueva bitacora

--- app/vistas/viajes/php/validador.php
+++ app/vistas/viajes/php/validador.php
@@ -984,7 +984,7 @@
 			break;
 		case 'traer_bitacora':
 			$html = '
-				<div class="row col-10 justify-content-center mx-5">
+				<div class="row col-10 justify-content-center mx-5 mt-3">
 				  <div class="col-lg-6 p-3 mb-2 bg-white  border">
 				    <label for="inputTitulo"><b>* Titulo:</b></label>
 				    <input type="text" class="form-control shadow" id="inputTipoServicio" placeholder="Escribir Tipo de servicio">

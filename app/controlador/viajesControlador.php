6837af960b4c8603356dfba25bc5dc1a802f19b2
Mauricio Villalobos Tordecilla <m.villalobos.tord@gmail.com>
2024-12-30 12:41:40 -0300

    panel flete

--- app/controlador/viajesControlador.php
+++ app/controlador/viajesControlador.php
@@ -1005,7 +1005,7 @@ ini_set('error_log', __DIR__ . '/php_errors.log');
 		                <h6>Fecha Arribo:</h6>
 		                '.Utilidades::arreglo_fecha2($datos_flete[$i]['fle_arribo']).'
 		            </div>
-		            <div class="col-xxl-6 col-xl-4 col-sm-12 pt-3 ">
+		            <div class="col-xxl-6 col-xl-6 col-sm-12 pt-3 ">
 		                <h6>Fecha Descarga:</h6>
 		                 '.Utilidades::arreglo_fecha2($datos_flete[$i]['fle_descarga']).'
 		            </div>';
@@ -1051,27 +1051,27 @@ ini_set('error_log', __DIR__ . '/php_errors.log');
 		                    </div>
 		                </div>
 		            </div>
-		            <div class="col-xxl-4 col-xl-6 col-sm-12 pt-3 ">
+		            <div class="col-xxl-6 col-xl-6 col-sm-12 pt-3 ">
 								<h6>Valor Viaje:</h6>
 								<span class="text-dark">
 								<h4 class="text-primary">'.Utilidades::monto3($datos_flete[$i]['fle_valor']).'</h4>
 						  		</span>
 							</div>
-							<div class="col-xxl-4 col-xl-6 col-sm-12 pt-3 ">
+							<div class="col-xxl-6 col-xl-6 col-sm-12 pt-3 ">
 								<h6>Descuentos:</h6>
 								<span class="text-dark">
 								<h4 class="text-primary">'.Utilidades::monto3($datos_flete[$i]['fle_descuento']).'</h4>
 						  		</span>
 							</div>
-							<div class="col-xxl-4 col-xl-6 col-sm-12 pt-3 ">
+							<div class="col-xxl-6 col-xl-6 col-sm-12 pt-3 ">
 								<h6>Total Viaje:</h6>
 								<h4 class="text-primary" id="total-viaje">' . Utilidades::monto3($datos_flete[$i]['fle_valor']-$datos_flete[$i]['fle_descuento']) . '</h4>
 							</div>
-							<div class="col-xxl-4 col-xl-6 col-sm-12 pt-3 ">
+							<div class="col-xxl-6 col-xl-6 col-sm-12 pt-3 ">
 								<h6>Estado de Pago:</h6>
 								'.$recursos->select_tipos_estados_pagos($datos_flete[$i]['fle_estado_pago']).'
 							</div>
-							<div class="col-xxl-4 col-xl-6 col-sm-12 pt-3 ">
+							<div class="col-xxl-6 col-xl-6 col-sm-12 pt-3 ">
 								<h6>Fecha de Viaje:</h6>
 								<span class="text-dark">
 								' . Utilidades::arreglo_fecha2($datos_flete[$i]['fle_fecha_pago']) . '

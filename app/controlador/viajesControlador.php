f61b712d5c35df20c605d72ab83a1d3a61c79f06
Mauricio Villalobos Tordecilla <m.villalobos.tord@gmail.com>
2024-12-30 11:49:12 -0300

    logo nuevo

--- app/controlador/viajesControlador.php
+++ app/controlador/viajesControlador.php
@@ -302,7 +302,7 @@ ini_set('error_log', __DIR__ . '/php_errors.log');
 								<td>
 									<small>
 										<div class="row">
-											<div class="col-10">
+											<div class="col">
 												 <div class="row">
 													<div class="col-6"><b>Tracto:</b></div>
 													<div class="col-6 text-primary"><b>'.$nombre_tracto.'</b></div>

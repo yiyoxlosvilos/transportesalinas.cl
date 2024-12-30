7124d954fb0fb89bc2d1495751c04585701270c4
Mauricio Villalobos Tordecilla <m.villalobos.tord@gmail.com>
2024-12-30 18:11:37 -0300

    nueva bitacora

--- app/vistas/viajes/php/validador.php
+++ app/vistas/viajes/php/validador.php
@@ -1,4 +1,6 @@
 <?php 
+	error_reporting(E_ALL);
+  	ini_set('display_errors', 1);
 	
 	date_default_timezone_set("America/Santiago");
 	require_once __dir__."/../../../controlador/controlador.php";

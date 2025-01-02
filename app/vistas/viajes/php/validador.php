04fbb34263303ed145b244cb4d10b1a83e0af719
Mauricio Villalobos Tordecilla <m.villalobos.tord@gmail.com>
2025-01-02 16:29:22 -0300

    documentos viajes

--- app/vistas/viajes/php/validador.php
+++ app/vistas/viajes/php/validador.php
@@ -1354,10 +1354,11 @@
 		            </div>
 		         	<!-- end card -->
 				  </div>
-				  <div class="col-lg-6 p-3 mb-2 bg-white  border">
+				  <div class="col-lg-6 p-3 mb-2 bg-white">
 				    <div class="row">
-				    	<div class="col">
-				    		<button class="btn btn-danger fas fa-file-pdf text-white h2" href="'.controlador::$rutaAPP.'/app/vistas/viajes/php/viajes_ver.php?idCotizacion='.$datos_fletes[0]['fle_id'].'" data-fancybox="" data-type="iframe" data-preload="true" data-width="1200" data-height="800"> Comprobante</button>
+				    	<div class="col justify-content-center">
+				    		<h4 class="text-primary">Descargar e Imprimir Comprobante</h4>
+				    		<button class="btn btn-danger fas fa-file-pdf text-white h2" href="'.controlador::$rutaAPP.'/app/vistas/viajes/php/viajes_ver.php?idCotizacion='.$datos_fletes[0]['fle_id'].'" data-fancybox="" data-type="iframe" data-preload="true" data-width="1200" data-height="800"></button>
 				    	</div>
 				  </div>
 	

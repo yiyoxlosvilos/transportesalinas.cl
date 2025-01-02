9d94d2236bcf01fcf6f7a8a66fa70e922ef81f49
Mauricio Villalobos Tordecilla <m.villalobos.tord@gmail.com>
2025-01-02 08:44:38 -0300

    documentos viajes

--- app/vistas/viajes/php/panel_flete.php
+++ app/vistas/viajes/php/panel_flete.php
@@ -157,36 +157,31 @@
               </div>
             </div>
             <div class="tab-pane fade" id="team-project" role="tabpanel" aria-labelledby="team-project-tab">
-                        <div class="row">
-                          <div class="col-8">
-                            <h3 class="text-dark mb-2"><i class="mdi mdi-format-list-bulleted"></i> Ingresar Pagos</h3>
+              <div class="row">
+                <div class="col-8">
+                  <h3 class="text-dark mb-2"><i class="mdi mdi-format-list-bulleted"></i> Ingreso de Pagos</h3>
+                </div>
+                <div class="col-md-15 mb-2">
+                  <div class="row">
+                    <div class="col-xl-3 col-md-6">
+                      <!-- card -->
+                      <div class="card card-h-100">
+                        <!-- card body -->
+                        <div class="card-body">
+                          <div class="row align-items-center">
+                            <div class="col-6">
+                              <span class="text-muted mb-3 lh-1 d-block text-truncate">Valor Viaje</span>
+                              <h4 class="mb-3">
+                                <span class="counter-value" data-target="<?= $datos_fletes[0]['fle_valor'] ?>"><?= Utilidades::monto3($datos_fletes[0]['fle_valor']) ?></span>
+                              </h4>
+                            </div>
                           </div>
-                          <div class="col-md-15 mb-2">
-                            <div class="row">
-                              <div class="col-xl-3 col-md-6">
-                                <!-- card -->
-                                <div class="card card-h-100">
-                                    <!-- card body -->
-                                    <div class="card-body">
-                                        <div class="row align-items-center">
-                                            <div class="col-6">
-                                                <span class="text-muted mb-3 lh-1 d-block text-truncate">My Wallet</span>
-                                                <h4 class="mb-3">
-                                                    $<span class="counter-value" data-target="865.2">865.2</span>k
-                                                </h4>
-                                            </div>
-                                            <div class="resize-triggers"><div class="expand-trigger"><div style="width: 136px; height: 59px;"></div></div><div class="contract-trigger"></div></div></div>
-                                        </div>
-                                        <div class="text-nowrap">
-                                            <span class="badge bg-soft-success text-success">+$20.9k</span>
-                                            <span class="ms-1 text-muted font-size-13">Since last week</span>
-                                        </div>
-                                    </div><!-- end card body -->
-                                </div><!-- end card -->
-                            </div>
-                            </div>
-                          </div>  
-                        </div>
+                        </div><!-- end card body -->
+                      </div><!-- end card -->
+                    </div>
+                  </div>
+                </div>  
+              </div>
             </div>
             <div class="tab-pane fade active" id="attachment" role="tabpanel" aria-labelledby="attachment-tab">
                         <div class="row">

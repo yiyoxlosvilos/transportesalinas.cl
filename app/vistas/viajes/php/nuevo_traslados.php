a6ba35de73c5ba453c80c0269fd55e3cd412e302
Mauricio Villalobos Tordecilla <m.villalobos.tord@gmail.com>
2025-01-02 19:25:22 -0300

    documentos viajes

--- app/vistas/viajes/php/nuevo_traslados.php
+++ app/vistas/viajes/php/nuevo_traslados.php
@@ -22,7 +22,7 @@
   <p align="left" class="text-info font-weight-light h3"><i class="fas fa-bus text-info"></i>&nbsp;&nbsp; Ingresar nuevo traslado</p>
   <hr class="mt-2 mb-3"/>
     <div class="container mb-4 shadow p-4">
-      <div class="row col-10">
+      <div class="row col-10 mx-3">
         <div class="col-xxl-4 col-xl-4 col-sm-12 p-3 mb-2 bg-white  border">
           <b>* Origen:</b>
           <span class="text-dark">

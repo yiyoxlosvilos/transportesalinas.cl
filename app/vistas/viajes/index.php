1d9a46f395ff786d098e0f6a59b16efa8b7aca09
Mauricio Villalobos Tordecilla <m.villalobos.tord@gmail.com>
2025-01-02 17:39:08 -0300

    documentos viajes

--- app/vistas/viajes/index.php
+++ app/vistas/viajes/index.php
@@ -31,113 +31,117 @@
   <div class="row paddingtop40px mt-5">
     <div class="container-fluid main-scope-project">
       <div class="row scope-bottom-wrapper">
-              <div class="col-xxl-2 recent-xl-23 col-xl-3 box-col-3">
-                <div class="card"> 
-                  <div class="card-body">
-                    <ul class="sidebar-left-icons nav nav-pills" id="add-product-pills-tab" role="tablist">
-                      <li class="nav-item" role="presentation"> 
-                        <a class="nav-link active" id="overview-project-tab" data-bs-toggle="pill" href="#overview-project" role="tab" aria-controls="overview-project" aria-selected="false" tabindex="-1">
-                          <div class="absolute-border"></div>
-                          <div class="nav-rounded">
-                            <div class="product-icons">
-                              <i class="fas fa-shipping-fast h6"></i>
-                            </div>
-                          </div>
-                          <div class="product-tab-content">
-                            <h6>Viajes</h6>
-                          </div></a>
-                      </li>
-                      <li class="nav-item" role="presentation"> <a class="nav-link" id="target-project-tab" data-bs-toggle="pill" href="#target-project" role="tab" aria-controls="target-project" aria-selected="false" tabindex="-1">
-                          <div class="absolute-border"></div>
-                          <div class="nav-rounded">
-                            <div class="product-icons">
-                              <i class="fas fa-bus h6"></i>
-                            </div>
-                          </div>
-                          <div class="product-tab-content">
-                            <h6>Traslados</h6>
-                          </div></a></li>
-                      <li class="nav-item" role="presentation"> <a class="nav-link" id="budget-project-tab" data-bs-toggle="pill" href="#budget-project" role="tab" aria-controls="budget-project" aria-selected="false" tabindex="-1">
-                          <div class="absolute-border"></div>
-                          <div class="nav-rounded">
-                            <div class="product-icons">
-                              <i class="fas fa-truck h6"></i>
-                            </div>
-                          </div>
-                          <div class="product-tab-content">
-                            <h6>Arriendos</h6>
-                          </div></a></li>
-                      <li class="nav-item" role="presentation"><a class="nav-link" id="team-project-tab" data-bs-toggle="pill" href="#team-project" role="tab" aria-controls="team-project" aria-selected="false" tabindex="-1">
-                          <div class="absolute-border"></div>
-                          <div class="nav-rounded">
-                            <div class="product-icons">
-                             <i class="fas fa-dollar-sign h6"></i>
-                            </div>
-                          </div>
-                          <div class="product-tab-content">
-                            <h6>Gastos</h6>
-                          </div></a></li>
-                      <li class="nav-item" role="presentation"><a class="nav-link " id="attachment-tab" data-bs-toggle="pill" href="#attachment" role="tab" aria-controls="attachment" aria-selected="true">
-                          <div class="absolute-border"></div>
-                          <div class="nav-rounded">
-                            <div class="product-icons">
-                              <i class="fas fa-receipt h6"></i>
-                            </div>
-                          </div>
-                          <div class="product-tab-content">
-                            <h6>Facturas Proveedores</h6>
-                          </div></a></li>
-                      <li class="nav-item" role="presentation"><a class="nav-link" id="activity-project-tab" data-bs-toggle="pill" href="#activity-project" role="tab" aria-controls="activity-project" aria-selected="false" tabindex="-1">
-                          <div class="absolute-border"></div>
-                          <div class="nav-rounded">
-                            <div class="product-icons">
-                              <i class="fas fa-receipt h6"></i>
-                            </div>
-                          </div>
-                          <div class="product-tab-content">
-                            <h6>Facturas Clientes</h6>
-                          </div></a></li>
-                    </ul>
+        <div class="col-xxl-2 recent-xl-23 col-xl-3 box-col-3">
+          <div class="card"> 
+            <div class="card-body">
+              <ul class="sidebar-left-icons nav nav-pills" id="add-product-pills-tab" role="tablist">
+                <li class="nav-item" role="presentation"> 
+                  <a class="nav-link active" id="overview-project-tab" data-bs-toggle="pill" href="#overview-project" role="tab" aria-controls="overview-project" aria-selected="false" tabindex="-1">
+                  <div class="absolute-border"></div>
+                  <div class="nav-rounded">
+                    <div class="product-icons">
+                      <i class="fas fa-shipping-fast h6"></i>
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div class="col-xxl-10 recent-xl-77 col-xl-9 box-col-9">
-                <div class="row">
-                  <div class="col-12">
-                    <div class="tab-content" id="add-product-pills-tabContent">
-                      <div class="tab-pane fade active show" id="overview-project" role="tabpanel" aria-labelledby="overview-project-tab">
-                        <div class="row">
-                          <div class="col-8">
-                            <h3 class="text-dark mb-2"><span class="mdi mdi-format-list-bulleted"></span> Detalles Viajes</h3>
-                          </div>
-                          <div class="col">
-                            <button class="btn btn-sm btn-dark" onclick="traer_menu('asignar_producto')"><i class="fas fa-shipping-fast text-white"></i> <span class="ocultar">Nuevo Viaje</span></button>
-                          </div>
-                          <div class="col-15 mt-3" id="traer_productos_categoria">
-                             <?= $centroCostos->traer_fletes_asigandos() ?>
-                          </div>
-    
-                        </div>
+                  <div class="product-tab-content">
+                    <h6>Viajes</h6>
+                  </div></a>
+                </li>
+                <li class="nav-item" role="presentation"> <a class="nav-link" id="target-project-tab" data-bs-toggle="pill" href="#target-project" role="tab" aria-controls="target-project" aria-selected="false" tabindex="-1">
+                    <div class="absolute-border"></div>
+                    <div class="nav-rounded">
+                      <div class="product-icons">
+                        <i class="fas fa-bus h6"></i>
                       </div>
-
-                      <div class="tab-pane fade" id="target-project" role="tabpanel" aria-labelledby="target-project-tab">
-                        <div class="col">
-                            <button class="btn btn-sm btn-dark" onclick="traer_menu('asignar_producto')"><i class="fas fa-shipping-fast text-white"></i> <span class="ocultar">Nuevo Traslado</span></button>
-                          </div>
-                          <div class="col-15 mt-3" id="traer_productos_categoria">
-                             <?= $centroCostos->listado_de_traslados() ?>
-                          </div>
-                      </div>
-                      <div class="tab-pane fade" id="budget-project" role="tabpanel" aria-labelledby="budget-project-tab">
-                      </div>
-                      <div class="tab-pane fade" id="team-project" role="tabpanel" aria-labelledby="team-project-tab">
-                      </div>
-                      <div class="tab-pane fade active show" id="attachment" role="tabpanel" aria-labelledby="attachment-tab"></div>
-                      <div class="tab-pane fade" id="activity-project" role="tabpanel" aria-labelledby="activity-project-tab"> </div>
+                    </div>
+                    <div class="product-tab-content">
+                      <h6>Traslados</h6>
+                    </div></a></li>
+                <li class="nav-item" role="presentation"> <a class="nav-link" id="budget-project-tab" data-bs-toggle="pill" href="#budget-project" role="tab" aria-controls="budget-project" aria-selected="false" tabindex="-1">
+                  <div class="absolute-border"></div>
+                  <div class="nav-rounded">
+                    <div class="product-icons">
+                      <i class="fas fa-truck h6"></i>
+                    </div>
+                  </div>
+                  <div class="product-tab-content">
+                    <h6>Arriendos</h6>
+                  </div></a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link" id="team-project-tab" data-bs-toggle="pill" href="#team-project" role="tab" aria-controls="team-project" aria-selected="false" tabindex="-1">
+                  <div class="absolute-border"></div>
+                  <div class="nav-rounded">
+                    <div class="product-icons">
+                     <i class="fas fa-dollar-sign h6"></i>
+                    </div>
+                  </div>
+                  <div class="product-tab-content">
+                    <h6>Gastos</h6>
+                  </div></a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link " id="attachment-tab" data-bs-toggle="pill" href="#attachment" role="tab" aria-controls="attachment" aria-selected="true">
+                  <div class="absolute-border"></div>
+                  <div class="nav-rounded">
+                    <div class="product-icons">
+                      <i class="fas fa-receipt h6"></i>
+                    </div>
+                  </div>
+                  <div class="product-tab-content">
+                    <h6>Facturas Proveedores</h6>
+                  </div></a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link" id="activity-project-tab" data-bs-toggle="pill" href="#activity-project" role="tab" aria-controls="activity-project" aria-selected="false" tabindex="-1">
+                  <div class="absolute-border"></div>
+                  <div class="nav-rounded">
+                    <div class="product-icons">
+                      <i class="fas fa-receipt h6"></i>
+                    </div>
+                  </div>
+                  <div class="product-tab-content">
+                    <h6>Facturas Clientes</h6>
+                  </div></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-xxl-10 recent-xl-77 col-xl-9 box-col-9">
+          <div class="row">
+            <div class="col-12">
+              <div class="tab-content" id="add-product-pills-tabContent">
+                <div class="tab-pane fade active show" id="overview-project" role="tabpanel" aria-labelledby="overview-project-tab">
+                  <div class="row">
+                    <div class="col-8">
+                      <h3 class="text-dark mb-2"><span class="mdi mdi-format-list-bulleted"></span> Detalles Viajes</h3>
+                    </div>
+                    <div class="col">
+                      <button class="btn btn-sm btn-dark" onclick="traer_menu('asignar_producto')"><i class="fas fa-shipping-fast text-white"></i> <span class="ocultar">Nuevo Viaje</span></button>
+                    </div>
+                    <div class="col-15 mt-3" id="traer_productos_categoria">
+                      <?= $centroCostos->traer_fletes_asigandos() ?>
                     </div>
                   </div>
                 </div>
+                <div class="tab-pane fade" id="target-project" role="tabpanel" aria-labelledby="target-project-tab">
+                  <div class="row">
+                    <div class="col-8">
+                        <h3 class="text-dark mb-2"><span class="mdi mdi-format-list-bulleted"></span> Detalles Traslados</h3>
+                      </div>
+                    <div class="col">
+                      <button class="btn btn-sm btn-dark" onclick="traer_menu('asignar_producto')"><i class="fas fa-shipping-fast text-white"></i> <span class="ocultar">Nuevo Traslado</span></button>
+                    </div>
+                    <div class="col-15 mt-3" id="traer_productos_categoria">
+                      <?= $centroCostos->listado_de_traslados() ?>
+                    </div>
+                  </div>
+                </div>
+                <div class="tab-pane fade" id="budget-project" role="tabpanel" aria-labelledby="budget-project-tab">
+                </div>
+                <div class="tab-pane fade" id="team-project" role="tabpanel" aria-labelledby="team-project-tab">
+                </div>
+                <div class="tab-pane fade active show" id="attachment" role="tabpanel" aria-labelledby="attachment-tab"></div>
+                <div class="tab-pane fade" id="activity-project" role="tabpanel" aria-labelledby="activity-project-tab"> </div>
               </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>

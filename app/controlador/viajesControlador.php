9f6f1c455f888b02d3caa656d90e64bb73049180
Mauricio Villalobos Tordecilla <m.villalobos.tord@gmail.com>
2025-01-03 01:42:57 -0300

    documentos viajes

--- app/controlador/viajesControlador.php
+++ app/controlador/viajesControlador.php
@@ -3784,7 +3784,7 @@ ini_set('error_log', __DIR__ . '/php_errors.log');
 	    	$html = '<table id="listado_arriendos" class="table shadow">
 	    				<thead >
 			              	<tr class="table-info">
-								<th colspan="3">1&nbsp;</th>
+								<th colspan="3">&nbsp;</th>
 							</tr>
 			            </thead>
 			            <tbody>';
@@ -3819,6 +3819,13 @@ ini_set('error_log', __DIR__ . '/php_errors.log');
 			}
 
 				$html .= '</tbody>
+						<tfooter>
+						<tr>
+							<th>&nbsp;</th>
+							<th align="right" >TOTAL:</th>
+							<th align="left">'.Utilidades::monto($total).'</th>
+						</tr>
+						</tfooter>
 					  </table>';
 
 

7ecde3ac6410f25b0218c242e2988039728c1c8d
Mauricio Villalobos Tordecilla <m.villalobos.tord@gmail.com>
2025-01-03 18:55:14 -0300

    documentos viajes

--- app/vistas/viajes/index.php
+++ app/vistas/viajes/index.php
@@ -170,7 +170,7 @@
                 </div>
                 <div class="tab-pane fade" id="budget-project" role="tabpanel" aria-labelledby="budget-project-tab">
                   <div class="row">
-                    <div class="col-">
+                    <div class="col-6">
                       <h3 class="text-dark mb-2"><span class="mdi mdi-format-list-bulleted"></span> Detalles Arriendo</h3>
                     </div>
                     <div class="col-3 mb-1">

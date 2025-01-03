9f12a1b670f16087de966a84a90448a02a68e8ad
Mauricio Villalobos Tordecilla <m.villalobos.tord@gmail.com>
2025-01-02 22:24:25 -0300

    documentos viajes

--- app/controlador/viajesControlador.php
+++ app/controlador/viajesControlador.php
@@ -4602,7 +4602,9 @@ ini_set('error_log', __DIR__ . '/php_errors.log');
 										<div class="col-6">';
 					$cadena_traslados = '';
 					for ($d=0; $d < count($explorar_origen); $d++) {
-						$cadena_traslados .= $recursos->nombre_localidad($explorar_origen[$d]).',';
+						if($explorar_origen[$d] != ''){
+							$cadena_traslados .= $recursos->nombre_localidad($explorar_origen[$d]).',';
+						}
 					}
 
 					$traslados .= preg_replace('/,$/', '', $cadena_traslados);
